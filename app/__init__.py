fe7e6772ec2d7907934667175ad93f5e0733256d
cBryan <cbbryan8@gmail.com>
2018-07-21 09:30:22 +0300

    [Chore #159165124] Import views to application factory

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,5 @@ from instance.config import config_environment
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(config_environment['development'])
 app.config.from_pyfile('config.py')
+
+from app import api
