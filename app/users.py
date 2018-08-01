2fcb01fa4864e623cd8dbc8fcbfef70ebf9f31fc
cBryan <cbbryan8@gmail.com>
2018-08-01 18:22:05 +0300

    [Feature #159468013] Add signin and login routes

---
+++ app/users.py
@@ -0,0 +1,49 @@
+from flask import request, jsonify, abort
+from app import app
+from flask.views import MethodView
+from models.models import User
+
+
+class UserRegistration(MethodView):
+
+    def post(self):
+        request_data = request.get_json()
+        username = request_data['username']
+        email = request_data['email']
+        password = request_data['password']
+        user = User(username=username, email=email, password=password)
+        user.add_user()
+        return jsonify({'message': 'user created'}), 201
+
+
+class UserLogin(MethodView):
+
+    def post(self):
+        request_data = request.get_json()
+        print(request_data)
+        if 'username' and 'password' not in request_data:
+            return jsonify({'message': 'Provide all the necessary credentials'}), 404
+        if len(request_data['username']) > 4:
+            user_candidate = request_data['username']
+            user = User.get_user_by_username(user_candidate)
+            if not user:
+                return jsonify({'message': 'Log in unsuccessful'})
+            if user[1] == user_candidate:
+                print('true')
+                password_candidate = request_data['password']
+                print(user)
+                user_password = user[-1]
+                if password_candidate == user_password:
+                    return jsonify({'message': 'log in success'}), 200
+            else:
+                return jsonify({'message': 'Log in unsuccessful'}), 404
+        else:
+            return jsonify({'Message': 'username must satisfy the minimum length'}), 400
+
+
+user_registration = UserRegistration.as_view('user_signup')
+user_login = UserLogin.as_view('user_login')
+app.add_url_rule('/auth/signup', view_func=user_registration, methods=['POST'])
+app.add_url_rule('/auth/signup', view_func=user_registration, methods=['GET'])
+app.add_url_rule('/auth/login', view_func=user_login, methods=['POST'])
+
--- models/models.py
+++ models/models.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
 from .create_db import connect_to_db
 import jwt
-from app import app
-
+import psycopg2.extras as extras
 conn = connect_to_db()
 
 
@@ -16,21 +15,30 @@ class User:
 
     def add_user(self):
         """Adds user new user db user"""
-        sql = """INSERT INTO users(username, email, password) VALUES(%s,%s,%s);"""
+        sql = """INSERT INTO users(username, email, password) VALUES(%s,%s,%s)"""
         cur = conn.cursor()
         cur.execute(sql, (self.username, self.email, self.password))
         conn.commit()
 
     @staticmethod
-    def get_users():
+    def get_user_by_username(username):
         """Method to read the user table and return the users"""
-        sql = """SELECT * FROM users"""
+        sql = """SELECT * FROM users WHERE username=%s"""
         cur = conn.cursor()
-        cur.execute(sql)
-        data = cur.fetchall()
+        cur.execute(sql, (username,))
+        data = cur.fetchone()
         return data
 
-    def generate_auth_token(user_id):
+    @staticmethod
+    def get_user_by_email(email):
+        """Method to get user by id to enable token generation"""
+        sql = """SELECT * FROM users WHERE email=(%s)"""
+        cur = conn.cursor()
+        cur.execute(sql, (email,))
+        user = cur.fetchone()
+        return user
+
+    def generate_auth_token(self, user_id):
         """
         Generates the authentication token
         :param user_id:
@@ -65,3 +73,36 @@ class User:
         except jwt.InvalidTokenError:
             return 'Invalid token. Please log in again'
 
+
+class Entry:
+
+    def __init__(self, title, journal):
+        self.title = title
+        self.journal = journal
+        self.create_at = datetime.now()
+        self.last_modified_at = datetime.now()
+
+    def save_entry(self):
+        """Method to save an entry into the database"""
+        conn.commit()
+
+    def add_entry(self):
+        """Method to add an entry into the database"""
+        sql = """INSERT INTO public.entries
+                (user_id, title, journal, create_at, last_modified_at)
+                VALUES('', '', '', '');"""
+        cur = conn.cursor()
+        cur.execute(sql, (self.title, self.journal, self.create_at, self.last_modified_at))
+        self.save_entry()
+
+    def get_entry_by_id(self):
+        """Method to return an entry by the id passed"""
+        pass
+
+    def edit_entry(self):
+        """Method to edit a specific entry"""
+        pass
+
+    def delete_entry(self):
+        """Method to remove an entry from the database"""
+        pass
